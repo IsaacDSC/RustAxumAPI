56ce9a2ea1348be30a8832a4465f190a374df0f4
IsaacDSC <isaac8.silva@hotmail.com>
2023-09-26 19:49:26 -0300

    chore: separete logic connection database

---
+++ api/src/configs/database.rs
@@ -0,0 +1,22 @@
+use sqlx::{Pool, Postgres, postgres::PgPoolOptions};
+
+
+
+pub async fn conn()-> Pool<Postgres>{
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool = match PgPoolOptions::new()
+        .max_connections(10)
+        .connect(&database_url)
+        .await
+    {
+        Ok(pool) => {
+            println!("✅Connection to the database is successful!");
+            pool
+        }
+        Err(err) => {
+            println!("🔥 Failed to connect to the database: {:?}", err);
+            std::process::exit(1);
+        }
+    };
+    return  pool;
+}
--- api/src/main.rs
+++ api/src/main.rs
@@ -1,8 +1,9 @@
-mod route;
+mod configs;
 mod handler;
-mod schema;
 mod model;
 mod repository;
+mod route;
+mod schema;
 mod service;
 
 use axum::http::{
@@ -22,28 +23,13 @@ pub struct AppState {
 async fn main() {
     dotenv().ok();
 
-    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let pool = match PgPoolOptions::new()
-        .max_connections(10)
-        .connect(&database_url)
-        .await
-    {
-        Ok(pool) => {
-            println!("✅Connection to the database is successful!");
-            pool
-        }
-        Err(err) => {
-            println!("🔥 Failed to connect to the database: {:?}", err);
-            std::process::exit(1);
-        }
-    };
+    let pool = configs::database::conn().await;
 
     let cors = CorsLayer::new()
         .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
         .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::DELETE])
         .allow_credentials(true)
         .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
-
 
     let app = route::start_routes(Arc::new(AppState { db: pool.clone() })).layer(cors);
     println!("[ * ] 🚀 Server started successfully http://localhost:3000");
